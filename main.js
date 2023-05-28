56167fb4db4fa285991afa14458c99f2a1148990
parthred <92373008+parthred@users.noreply.github.com>
2023-05-28 19:09:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent('canvas');
+}
+
+function draw() {
+	game()
+}
+function startGame() 
+{
+	console.log("gameloaded");
+
+}
+
+
+
+
+
+
